632f4736b9eb58bfb6388196d44441b84c55a487
mdsjjorge <mdsousa.jorge@gmail.com>
2022-07-15 17:01:48 -0300

    adiciona interface inicial

---
+++ main.js
@@ -0,0 +1,11 @@
+// let totalScore 
+
+// if (totalScore >= 75) {
+//     return "Zona de excelência";
+// } else if (totalScore >= 50) {
+//     return "Zona de qualidade";
+// } else if (totalScore >= 0) {
+//     return "Zona de aperfeiçoamento";
+// } else {
+//     return "Zona de crítica";
+// }
