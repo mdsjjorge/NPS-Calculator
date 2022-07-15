bcc7663c0e92198d365f2eb0170b56f5ad57b4c3
mdsjjorge <mdsousa.jorge@gmail.com>
2022-07-15 20:40:22 -0300

    lógica inicial

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
+const calculate = () => {
+
+    let totalSum = 0;
+    let promoterSum = 0;
+    let promoterPercent = 0;
+    let detractorSum = 0;
+    let detractorPercent = 0;
+    let totalScore = 0;
+
+    for (let i = 0; i <= 10; i++) {
+        let numbers = document.querySelector(`#num${i}`);
+        let number = Number(numbers.value);
+        // console.log(`number[${i}] = ${number}`);  
+        totalSum += number;  
+        // console.log(`totalSum = ${totalSum}`);
+        if (i > 8) {
+            let promoterNumber = Number(numbers.value);
+            // console.log(`promoterNumber[${i}] = ${promoterNumber}`);
+            promoterSum += promoterNumber;
+
+        } else if (i < 7) {
+            let detractorNumber = Number(numbers.value);
+            // console.log(`detractorNumber[${i}] = ${detractorNumber}`);
+            detractorSum += detractorNumber;
+        }
+    }
+    promoterPercent = promoterSum / totalSum;
+
+    detractorPercent = detractorSum / totalSum;
+
+    console.log(`detractorPercent = ${detractorPercent}`);
+    console.log(`promoterPercent = ${promoterPercent}`);
+    totalScore = promoterPercent - detractorPercent
+    console.log(`totalScore = ${totalScore}`);
+
+}
+
+
+
 // let totalScore 
 
 // if (totalScore >= 75) {
