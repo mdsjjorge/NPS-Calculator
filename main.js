23f8afb174eccd8913db6d544b210e32508ee3b5
mdsjjorge <mdsousa.jorge@gmail.com>
2022-07-18 11:42:41 -0300

    innerHTML no result para cada condição

--- main.js
+++ main.js
@@ -6,45 +6,38 @@ const calculate = () => {
     let detractorSum = 0;
     let detractorPercent = 0;
     let totalScore = 0;
+    let result = document.querySelector('#result');
 
     for (let i = 0; i <= 10; i++) {
+
         let numbers = document.querySelector(`#num${i}`);
         let number = Number(numbers.value);
-        // console.log(`number[${i}] = ${number}`);  
         totalSum += number;  
-        // console.log(`totalSum = ${totalSum}`);
+
         if (i > 8) {
             let promoterNumber = Number(numbers.value);
-            // console.log(`promoterNumber[${i}] = ${promoterNumber}`);
             promoterSum += promoterNumber;
-
         } else if (i < 7) {
             let detractorNumber = Number(numbers.value);
-            // console.log(`detractorNumber[${i}] = ${detractorNumber}`);
             detractorSum += detractorNumber;
         }
     }
-    promoterPercent = promoterSum / totalSum;
 
-    detractorPercent = detractorSum / totalSum;
+    promoterPercent = (promoterSum / totalSum) * 100;
+    detractorPercent = (detractorSum / totalSum) * 100;
+    totalScore = (promoterPercent - detractorPercent).toFixed(2);
 
-    console.log(`detractorPercent = ${detractorPercent}`);
-    console.log(`promoterPercent = ${promoterPercent}`);
-    totalScore = promoterPercent - detractorPercent
-    console.log(`totalScore = ${totalScore}`);
+    // console.log(`detractorPercent = ${detractorPercent}`);
+    // console.log(`promoterPercent = ${promoterPercent}`);
+    // console.log(`totalScore = ${totalScore}`);
 
+    if (totalScore >= 75) {
+        result.innerHTML = `excellence zone, score: ${totalScore}`;
+    } else if (totalScore >= 50) {
+        result.innerHTML = `quality zone, score: ${totalScore}`;
+    } else if (totalScore >= 0) {
+        result.innerHTML = `improvement zone, score: ${totalScore}`;
+    } else {
+        result.innerHTML = `critical zone, score: ${totalScore}`;
+    }
 }
-
-
-
-// let totalScore 
-
-// if (totalScore >= 75) {
-//     return "Zona de excelência";
-// } else if (totalScore >= 50) {
-//     return "Zona de qualidade";
-// } else if (totalScore >= 0) {
-//     return "Zona de aperfeiçoamento";
-// } else {
-//     return "Zona de crítica";
-// }
