9a79a0f8187d1c789def637d91ccc68904c55c0b
mdsjjorge <mdsousa.jorge@gmail.com>
2022-07-22 08:06:55 -0300

    substitui evento click por onmouseout e altera a forma de mostrar resultados

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const calculate = () => {
     let detractorSum = 0;
     let detractorPercent = 0;
     let totalScore = 0;
-    let result = document.querySelector('#result');
+    let resultScore = document.querySelector('.resultScore');
+    let resultZone = document.querySelector('.resultZone');
+    let resultText = document.querySelector('.resultText');
 
     for (let i = 0; i <= 10; i++) {
 
@@ -32,13 +34,23 @@ const calculate = () => {
     // console.log(`promoterPercent = ${promoterPercent}`);
     // console.log(`totalScore = ${totalScore}`);
 
-    if (totalScore >= 75) {
-        result.innerHTML = `excellence zone, score: ${totalScore}`;
-    } else if (totalScore >= 50) {
-        result.innerHTML = `quality zone, score: ${totalScore}`;
-    } else if (totalScore >= 0) {
-        result.innerHTML = `improvement zone, score: ${totalScore}`;
-    } else {
-        result.innerHTML = `critical zone, score: ${totalScore}`;
-    }
+    if (totalSum != 0) {
+        if (totalScore >= 75) {
+            resultScore.innerHTML = `Nota: ${totalScore}`;
+            resultZone.innerHTML = "Zona: Excelência";
+            resultText.innerHTML = "Comentário: Parabéns, sua empresa satisfaz seus clientes, atende às suas expectativas e garante uma boa experiência ao usuário, conquista consumidores fiéis que recomendam e defendem a marca."
+        } else if (totalScore >= 50) {
+            resultScore.innerHTML = `Nota: ${totalScore}`;
+            resultZone.innerHTML = "Zona: Qualidade";
+            resultText.innerHTML = "Comentário: Sua empresa conta com clientes satisfeitos com a marca, mas que ressaltam pontos que deixaram a desejar. Realizar uma avaliação da jornada do cliente é importante para que a empresa possa identificar possíveis falhas.";
+        } else if (totalScore >= 0) {
+            resultScore.innerHTML = `Nota: ${totalScore}`;
+            resultZone.innerHTML = "Zona: aperfeiçoamento";
+            resultText.innerHTML = "Comentário: Sua empresa deve ter muita atenção com os feedbacks dos seus clientes. Estar situado nesta zona significa que muitos clientes não tiveram uma boa experiência e não ficaram satisfeitos com a empresa. Refazer processos, melhorar estratégias e conhecer melhor as espectativas dos clientes são pontos importantes a serem avaliados.";
+        } else {
+            resultScore.innerHTML = `Nota: ${totalScore}`;
+            resultZone.innerHTML = "Zona: Crítica";
+            resultText.innerHTML = "Comentário: Sua empresa deve tomar ações rápidas em relação às suas estratégias. Essa categoria indica que muitos clientes ficaram insatisfeitos com a empresa e que não recomendariam a marca. Eles serão os primeiros a reclamarem, a divulgarem feedbacks negativos sobre suas experiências, podendo engajar uma comunidade de outros clientes.";
+        }
+    }    
 }
